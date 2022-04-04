9986187f9331d550e63db02ca7e274d17be26c9b
Alfredo <aacanizales.13@est.ucab.edu.ve>
2022-04-04 10:05:50 -0300

    list and search of musical content

--- src/app/music-information/list-music-information/list-music-information.component.ts
+++ src/app/music-information/list-music-information/list-music-information.component.ts
@@ -1,12 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MusicInformationService } from '../music-information.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { FormControl } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { NotificationsService } from '../../global/services/notifications.service';
 
 @Component({
   selector: 'app-list-music-information',
   templateUrl: './list-music-information.component.html',
   styleUrls: ['./list-music-information.component.scss'],
 })
-export class ListMusicInformationComponent implements OnInit {
-  constructor() {}
+export class ListMusicInformationComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+  displayedColumns: string[];
+  resultsLength: number;
+  dataSource: MatTableDataSource<any>;
+  filter: string | undefined;
+
+  constructor(
+    private _musicInformationService: MusicInformationService,
+    private _notificationsService: NotificationsService
+  ) {
+    this.displayedColumns = ['name', 'artists'];
+    this.resultsLength = 0;
+    this.dataSource = new MatTableDataSource<any>();
+    this.dataSource.data = [];
+  }
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.initList();
+  }
+  initList(payload?: any) {
+    this._musicInformationService.search(payload ? payload : null).subscribe(
+      (response) => {
+        this.dataSource = new MatTableDataSource<any>(response.tracks.items);
+        this.resultsLength = response.tracks.total;
+        if (!payload) {
+          this.dataSource.paginator = this.paginator ? this.paginator : null;
+        }
+      },
+      (error) => {
+        if (error.status === 400) {
+          this._notificationsService.showNotification('danger', 'No hay resultados actualmente');
+        } else {
+          this._notificationsService.showNotification('danger', 'Ha ocurrido un error');
+        }
+      }
+    );
+  }
+  handlePageEvent(event: any) {
+    let payload = {
+      filter: this.filter,
+      index: event.pageIndex * event.pageSize,
+    };
+    this.initList(payload);
+  }
+  textFilter(event: any) {
+    let payload = {
+      filter: this.filter,
+      index: 0,
+    };
+    this.initList(payload);
+  }
 }
--- src/app/music-information/music-information.module.ts
+++ src/app/music-information/music-information.module.ts
@@ -5,9 +5,20 @@ import { ListMusicInformationComponent } from './list-music-information/list-mus
 import { RouterModule } from '@angular/router';
 import { AuthRoutes } from '../auth/auth.routing';
 import { MusicInformationRoutes } from './music-information.routing';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [MusicInformationComponent, ListMusicInformationComponent],
-  imports: [CommonModule, RouterModule.forChild(MusicInformationRoutes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(MusicInformationRoutes),
+    MatPaginatorModule,
+    MatTableModule,
+    MatInputModule,
+    FormsModule,
+  ],
 })
 export class MusicInformationModule {}
